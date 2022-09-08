8f78ae0b8acc82146b8ecc4881a91a853cda0ba4
dev-ahmedali <ahmedali69325@gmail.com>
2022-09-08 13:50:02 +0600

    added question

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        number: 1,
+        question: "An HTML document can contain?",
+        answer: "All the answers are true",
+        options: [
+            "Attributes",
+            "Tags",
+            "Raw text",
+            "All the answers are true"
+        ]
+    },
+    {
+        number: 2,
+        question: "CSS stands for -",
+        answer: "Cascading style sheets",
+        options: [
+            "Cascade style sheets",
+            "Color and style sheets",
+            "Cascading style sheets",
+            "None of the above"
+        ]
+    },
+    {
+        number: 3,
+        question: "Choose the correct HTML tag for a large title.",
+        answer: "H1",
+        options: [
+            "H1",
+            "Heading",
+            " H6",
+            "Head"
+        ]
+    },
+    {
+        number: 4,
+        question: "Which is not A XML function",
+        answer: "Style Information",
+        options: [
+            "Structure Information",
+            "Store Information",
+            "Style Information",
+            "Transport Information"
+        ]
+    },
+    {
+        number: 5,
+        question: "What is the default port where webpack-server runs?",
+        answer: "3000",
+        options: [
+            "6060",
+            "8080",
+            "3000",
+            "3030"
+        ]
+    },
+]
--- js/script.js
+++ js/script.js
@@ -1,12 +1,75 @@
 // Select element
-const quizButton = document.querySelector(".quizBtn button");
-const rulesBox = document.querySelector(".rules-box")
-const exitButton = document.querySelector(".buttons .exitBtn")
+const quizButton = document.querySelector('.quizBtn button');
+const rulesBox = document.querySelector('.rules-box');
+const exitButton = document.querySelector('.buttons .exitBtn');
+const continueButton = document.querySelector('.buttons .continueBtn');
+const Questions = document.querySelector('.questions');
+const questionText = document.querySelector('.quiz-text');
+const optionList = document.querySelector('.MyOptions');
+const NextButton = document.querySelector('.nextBtn');
+const totolQuestion = document.querySelector(".total_que")
 
-quizButton.onclick = ()=> {
-    rulesBox.classList.add("activeInfo")
+
+quizButton.onclick = () => {
+  rulesBox.classList.add('activeInfo');
+};
+
+exitButton.onclick = () => {
+  rulesBox.classList.remove('activeInfo');
+};
+
+continueButton.onclick = () => {
+  rulesBox.classList.remove('activeInfo');
+  Questions.classList.add('questionInfo');
+  showQuestion(0);
+};
+let questionCount = 0;
+
+NextButton.onclick = () => {
+    if (questionCount < questions.length - 1) {
+        questionCount++
+        showQuestion(questionCount)
+    } else {
+        alert("You have complete your task")
+    }
 }
 
-exitButton.onclick = ()=> {
-    rulesBox.classList.remove("activeInfo")
+function showQuestion(index) {
+  let optionTag =
+    '<div class="options">' +
+    questions[index].options[0] +
+    '</div>' +
+    '<div class="options">' +
+    questions[index].options[1] +
+    '</div>' +
+    '<div class="options">' +
+    questions[index].options[2] +
+    '</div>' +
+    '<div class="options">' +
+    questions[index].options[3] +
+    '</div>';
+  let questionTag =
+    '<span>' +
+    questions[index].number +
+    '.' +
+    questions[index].question +
+    '</span';
+  questionText.innerHTML = questionTag;
+  optionList.innerHTML = optionTag;
+  let totalQueTag = '<p>' +  questions[index].number + ' of 5 </p>'
+  totolQuestion.innerHTML = totalQueTag
+  const option = optionList.querySelectorAll(".options")
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)")
+  }
+}
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAns = questions[questionCount].answer;
+  if (userAnswer === correctAns) {
+    console.log('answer is correct')
+  } else {
+    console.log('Answer is wrong')
+  }
 }
