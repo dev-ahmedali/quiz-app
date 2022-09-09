946a10777edbf05beac2d11cd19a1221e53150ba
dev-ahmedali <ahmedali69325@gmail.com>
2022-09-09 12:53:45 +0600

    added option and timer

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ const Questions = document.querySelector('.questions');
 const questionText = document.querySelector('.quiz-text');
 const optionList = document.querySelector('.MyOptions');
 const NextButton = document.querySelector('.nextBtn');
-const totolQuestion = document.querySelector(".total_que")
-
+const totolQuestion = document.querySelector('.total_que');
+const timeCounter = document.querySelector('.timeCount .seconds');
+const timeLine = document.querySelector('.questionsHeader .time_lines');
 
 quizButton.onclick = () => {
   rulesBox.classList.add('activeInfo');
@@ -22,17 +23,30 @@ continueButton.onclick = () => {
   rulesBox.classList.remove('activeInfo');
   Questions.classList.add('questionInfo');
   showQuestion(0);
+  startTimer(15);
+  startTimerLine(0)
 };
+// defined variable
 let questionCount = 0;
+let counter;
+let timeValue = 15;
+let counterLine;
+let widthValue = 0
 
 NextButton.onclick = () => {
-    if (questionCount < questions.length - 1) {
-        questionCount++
-        showQuestion(questionCount)
-    } else {
-        alert("You have complete your task")
-    }
-}
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    showQuestion(questionCount);
+    clearInterval(counter);
+    startTimer(timeValue);
+
+    clearInterval(counterLine)
+    startTimerLine(widthValue)
+    NextButton.style.display = 'none'
+  } else {
+    alert('You have complete your task');
+  }
+};
 
 function showQuestion(index) {
   let optionTag =
@@ -56,20 +70,73 @@ function showQuestion(index) {
     '</span';
   questionText.innerHTML = questionTag;
   optionList.innerHTML = optionTag;
-  let totalQueTag = '<p>' +  questions[index].number + ' of 5 </p>'
-  totolQuestion.innerHTML = totalQueTag
-  const option = optionList.querySelectorAll(".options")
+  let totalQueTag = '<p>' + questions[index].number + ' of 5 </p>';
+  totolQuestion.innerHTML = totalQueTag;
+  const option = optionList.querySelectorAll('.options');
   for (let i = 0; i < option.length; i++) {
-    option[i].setAttribute("onclick", "optionSelected(this)")
+    option[i].setAttribute('onclick', 'optionSelected(this)');
   }
 }
 
+let checkIcon =
+  '<div class="tick icon"><i class="fa-solid fa-check"></i></div>';
+let crossIcon =
+  '<div class="cross icon"><i class="fa-solid fa-xmark"></i></div>';
+
 function optionSelected(answer) {
+  clearInterval(counterLine)
+  clearInterval(counter);
   let userAnswer = answer.textContent;
   let correctAns = questions[questionCount].answer;
+  let allOptions = optionList.children.length;
   if (userAnswer === correctAns) {
-    console.log('answer is correct')
+    answer.classList.add('correct');
+    console.log('answer is correct');
+    answer.insertAdjacentHTML('beforeend', checkIcon);
   } else {
-    console.log('Answer is wrong')
+    answer.classList.add('incorrect');
+    console.log('Answer is wrong');
+    answer.insertAdjacentHTML('beforeend', crossIcon);
+
+    for (let i = 0; i < allOptions; i++) {
+      if (optionList.children[i].textContent == correctAns) {
+        optionList.children[i].setAttribute('class', 'options correct');
+        optionList.children[i].insertAdjacentHTML('beforeend', checkIcon);
+      }
+    }
+  }
+
+  for (let i = 0; i < allOptions; i++) {
+    optionList.children[i].classList.add('disabled');
+  }
+  NextButton.style.display = 'block'
+}
+
+// Start timer
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCounter.textContent = time;
+    time--;
+    if (time < 9) {
+      let addzero = timeCounter.textContent;
+      timeCounter.textContent = '0' + addzero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeCounter.textContent = '00';
+    }
+  }
+}
+
+
+function startTimerLine(time) {
+  counterLine = setInterval(timer, 50)
+  function timer() {
+    time = time + 1;
+    timeLine.style.width = time + 'px'
+    if(time > 319) {
+      clearInterval(counterLine)
+    }
   }
 }
